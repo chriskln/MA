49d098fb36d3989e7868804dd1cc601eabfc64f9
chriskln <93598297+chriskln@users.noreply.github.com>
2022-05-06 18:07:52 +0200

    1

--- MA_test.py
+++ MA_test.py
@@ -1,11 +1,51 @@
-import csv
-from datetime import date, datetime, timedelta
+##############################################
+# MASTER THESIS
+##############################################
 
-with open("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyflow012019_012021.csv") as f:
-    for line in f:
-        data = line.strip().split(";")
-        data = [x.replace("Estimated Share Class Net Flow (Daily) ", "") for x in data]
-        data = [x.replace(" Base Currency", "") for x in data]
+##############################################
+# Modules & Functions
+##############################################
+
+import csv
+import numpy as np
+import pandas as pd
+
+##############################################
+# Loading Data
+##############################################
+
+
+df_flow = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyflow012019_012021.csv", sep = ";")
+df_return = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyreturn012019_012021.csv", sep = ";")
+
+
+##############################################
+# Code
+##############################################
+
+# control
+# print(df_flow.head)
+# print(df_return.head)
+
+# delete all funds with no flow data
+df_flow = df_flow.dropna(axis="index", how= "all", thresh=3)
+df_return = df_return.dropna(axis="index", how= "all", thresh=3)
+
+# delete all days with no flow data (closed stock market)
+df_flow = df_flow.dropna(axis="columns", how= "all")
+df_return = df_return.dropna (axis="columns", how="all")
+
+print(df_return)
+print(df_flow)
+
+
+
+#for line in df_flow:
+#    data = line.strip().split(";")
+#    data = [x.replace("Estimated Share Class Net Flow (Daily) ", "") for x in data]
+#    data = [x.replace(" Base Currency", "") for x in data]
+#    print(df_flow)
+#with open("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyflow012019_012021.csv") as f:
 
 
 
--- pandas_test.py
+++ pandas_test.py
@@ -2,4 +2,3 @@ import pandas as pd
 df = pd.read_csv ("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyflow012019_012021.csv", sep = ';')
 print(df)
 
-
