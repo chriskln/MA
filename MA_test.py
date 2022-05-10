f3a1e28e273b0d3d2cee922dc5ff77eee42f4ed5
chriskln <93598297+chriskln@users.noreply.github.com>
2022-05-10 18:21:16 +0200

    10.05.22

--- MA_test.py
+++ MA_test.py
@@ -15,8 +15,8 @@ import pandas as pd
 ##############################################
 
 
-df_flow = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyflow012019_012021.csv", sep = ";")
-df_return = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv\\dailyreturn012019_012021.csv", sep = ";")
+df_flow = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv_2\\Daily_flows_01.01.17_31.12.20.csv", sep = ";")
+#df_return = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\csv_2\\dailyreturn012019_012021.csv", sep = ";")
 
 
 ##############################################
@@ -28,14 +28,13 @@ df_return = pd.read_csv("C:\\Users\\klein\\OneDrive\\Dokumente\\Master Thesis\\c
 # print(df_return.head)
 
 # delete all funds with no flow data
-df_flow = df_flow.dropna(axis="index", how= "all", thresh=3)
-df_return = df_return.dropna(axis="index", how= "all", thresh=3)
+df_flow = df_flow.dropna(axis="index", how= "all", thresh=6)
+
 
 # delete all days with no flow data (closed stock market)
 df_flow = df_flow.dropna(axis="columns", how= "all")
-df_return = df_return.dropna (axis="columns", how="all")
 
-print(df_return)
+
 print(df_flow)
 
 
